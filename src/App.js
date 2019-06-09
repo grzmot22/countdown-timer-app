0091c0af54c94bc7bcf63cda101ec28344fbe0f1
Grzmot22 <grzmot22@gmail.com>
2019-06-09 12:03:21 +0100

    Adds analog clock and hooks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import FlipNumbers from 'react-flip-numbers';
+import Clock from 'react-clock';
 
 function App() {
-	return <div className="App" />;
+	const [ date, setDate ] = useState(new Date());
+	useEffect(() => {
+		setInterval(() => setDate(new Date()), 1000);
+	});
+	console.log(date);
+	return (
+		<div>
+			<FlipNumbers
+				height={12}
+				width={12}
+				color="red"
+				background="white"
+				play
+				perspective={100}
+				numbers={date.getHours().toString()}
+			/>
+			<FlipNumbers
+				height={12}
+				width={12}
+				color="red"
+				background="white"
+				play
+				perspective={100}
+				numbers={date.getMinutes().toString()}
+			/>
+			<FlipNumbers
+				height={12}
+				width={12}
+				color="red"
+				background="white"
+				play
+				perspective={100}
+				numbers={date.getSeconds().toString()}
+			/>
+			<Clock value={date} />
+		</div>
+	);
 }
 
 export default App;
