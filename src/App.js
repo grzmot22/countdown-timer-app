d04f9955cc8960babb57ef9227a17c6030456d44
Grzmot22 <grzmot22@gmail.com>
2021-05-30 12:18:00 +0200

    Updates packages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React, { useState, useEffect } from "react";
-import FlipNumbers from "react-flip-numbers";
-import Clock from "react-clock";
-import styled from "styled-components";
+import React, { useState, useEffect } from 'react';
+import 'react-clock/dist/Clock.css';
+import FlipNumbers from 'react-flip-numbers';
+import Clock from 'react-clock';
+import styled from 'styled-components';
 // import { makeStyles } from "@material-ui/styles";
 // import Button from '@material-ui/core/Button';
-import DateFnsUtils from "@material-ui/pickers/adapter/moment";
-import { LocalizationProvider, DateTimePicker } from "@material-ui/pickers";
-import TextField from "@material-ui/core/TextField";
-import moment from "moment";
+import DateFnsUtils from '@material-ui/pickers/adapter/moment';
+import { LocalizationProvider, DateTimePicker } from '@material-ui/pickers';
+import TextField from '@material-ui/core/TextField';
+import moment from 'moment';
 
 // const useStyles = makeStyles({
 //   root: {
-//     background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+//     background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
 //     border: 0,
 //     borderRadius: 3,
-//     boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
-//     color: "white",
+//     boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+//     color: 'white',
 //     height: 48,
-//     padding: "0 30px"
-//   }
+//     padding: '0 30px',
+//   },
 // });
 
 const NumberFlip = ({ number }) => (
@@ -36,29 +37,29 @@ const NumberFlip = ({ number }) => (
 const App = () => {
   const [date, setDate] = useState(new Date());
   const [targetDate, handleDateChange] = useState(
-    moment(moment()).add(30, "m").toDate()
+    moment(moment()).add(30, 'm').toDate(),
   );
   const intervalTime = 1000;
   const now = () => Date.now();
 
   const [timeRemaining, setTimeRemaining] = useState(
-    () => new Date(targetDate) - new Date(now())
+    () => new Date(targetDate) - new Date(now()),
   );
 
   const [seconds, setSeconds] = useState(
-    Math.floor((timeRemaining / 1000) % 60)
+    Math.floor((timeRemaining / 1000) % 60),
   );
 
   const [minutes, setMinutes] = useState(
-    Math.floor((timeRemaining / 1000 / 60) % 60)
+    Math.floor((timeRemaining / 1000 / 60) % 60),
   );
 
   const [hours, setHours] = useState(
-    Math.floor((timeRemaining / (1000 * 60 * 60)) % 24)
+    Math.floor((timeRemaining / (1000 * 60 * 60)) % 24),
   );
 
   const [days, setDays] = useState(
-    Math.floor(timeRemaining / (1000 * 60 * 60 * 24))
+    Math.floor(timeRemaining / (1000 * 60 * 60 * 24)),
   );
 
   // const classes = useStyles();
